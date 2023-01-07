b3fbe6c425dbe22207c3f9128c1f0a0c55beba77
Maycon Oliveira <flayner5@gmail.com>
2023-01-07 17:00:31 -0300

    Added the site's fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		fontFamily: {
+			body: ['Varela Round', ...defaultTheme.fontFamily.sans],
+			title: ['Nunito', ...defaultTheme.fontFamily.sans]
+		},
 		extend: {}
 	},
 	plugins: []
