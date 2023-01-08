c211a547bb0d387a7c8af9cbc726ac56e708af4e
Maycon Oliveira <flayner5@gmail.com>
2023-01-07 21:28:21 -0300

    Added the theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,28 @@
 
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+const themeYellow = {
+	bg: '#FEF6E1',
+	bgAlt: '#F4D8E4',
+	casorioRed: '#EA2F35',
+	casorioBlue: '#0178AE',
+	casorioYellow: '#F2C200',
+	casorioPink: '#F270A7',
+	casorioBlack: '#4D4A44'
+};
+
+const themePink = {
+	bg: '#F4D8E4',
+	bgAlt: '#FEF6E1',
+	casorioRed: '#E12935',
+	casorioBlue: '#016AB0',
+	casorioYellow: '#E8AA00',
+	casorioPink: '#E862A9',
+	casorioBlack: '#4A4144'
+};
+
+const theme = 'yellow';
+
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
@@ -9,7 +31,9 @@ module.exports = {
 			body: ['Varela Round', ...defaultTheme.fontFamily.sans],
 			title: ['Nunito', ...defaultTheme.fontFamily.sans]
 		},
-		extend: {}
+		extend: {
+			colors: theme == 'yellow' ? themeYellow : themePink
+		}
 	},
 	plugins: []
 };
