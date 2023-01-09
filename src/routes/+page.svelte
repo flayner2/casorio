69155192dcca0b8bbeadaa3d03db0361ad2d2378
Maycon Oliveira <flayner5@gmail.com>
2023-01-08 21:30:04 -0300

    Finished the landing page skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,18 @@
 	<title>MAYCOELA</title>
 </svelte:head>
 
-<div class="w-full min-h-[80vh] bg-red-500">
-	<p>test</p>
+<div
+	class="flex flex-intial items-center justify-around md:h-[80vh] mr-40 ml-36 max-w-[100vw] mt-[-2rem] 2xl:mt-[-4rem]"
+>
+	<div class="pl-10">
+		<img src="/home_img.png" alt="Test" class="h-full 2xl:h-[40em]" />
+	</div>
+	<div class="pl-10 w-1/2 2xl:w-2/5">
+		<p class="text-sm tracking-[0.09em] 2xl:leading-5 font-body text-casorioBlack uppercase">
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+			galley of type and scrambled it to make a type specimen book. It has survived not only five
+			centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+		</p>
+	</div>
 </div>
