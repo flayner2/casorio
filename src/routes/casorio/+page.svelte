52cd2a5b073ac651f14f20bb80368a1730bffaa2
Maycon Oliveira <flayner5@gmail.com>
2023-01-10 22:21:51 -0300

    Fixed the wide iPad version of the page

--- src/routes/casorio/+page.svelte
+++ src/routes/casorio/+page.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 
 <div
-	class="grid grid-rows-3 justify-items-center gap-y-10 lg:gap-y-0 lg:grid-cols-3 lg:grid-rows-1 w-full lg:pl-20 lg:pr-20 lg:h-[80vh]"
+	class="grid grid-rows-3 justify-items-center gap-y-10 lg:gap-y-0 lg:grid-cols-3 lg:grid-rows-1 w-full lg:pl-20 lg:pr-20 xl:h-[80vh] lg:h-[50vh]"
 >
 	<div
 		class="flex flex-col items-center w-[90%] lg:w-auto 2xl:max-h-[80vh] justify-center lg:mt-[-5em] space-y-4 lg:space-y-0"
@@ -18,9 +18,11 @@
 	<img
 		src="/mapa.png"
 		alt="Mapa do casamento"
-		class="w-[60%] lg:w-auto lg:h-auto lg:pl-4 lg:pr-4 2xl:mt-[-10rem] lg:mt-[-8rem]"
+		class="w-[60%] lg:w-auto lg:h-auto lg:pl-4 lg:pr-4 lg:self-center"
 	/>
-	<div class="flex flex-col items-start justify-center max-h-[80vh] mt-[-3em] 2xl:ml-20 md:ml-16">
+	<div
+		class="flex flex-col items-start justify-center max-h-[80vh] mt-[-3em] 2xl:ml-20 xl:ml-16 lg:ml-8"
+	>
 		<div class="flex flex-col items-start justify-between mb-10">
 			<div
 				class="flex justify-between items-center text-xl 2xl:text-2xl font-title text-casorioPink uppercase font-bold mb-3 2xl:mb-5"
