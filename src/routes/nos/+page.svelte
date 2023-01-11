df2c93c1df3e20eb51dd5cd423e05b202f4c1c3e
Maycon Oliveira <flayner5@gmail.com>
2023-01-10 22:20:23 -0300

    Fixed some issues with the desktop layouts introduced in the previous edit

--- src/routes/nos/+page.svelte
+++ src/routes/nos/+page.svelte
@@ -93,7 +93,7 @@
 	<div
 		class="group border-[1px] border-casorioBlack row-start-1 row-end-3 col-start-1 col-end-2 w-[95%] h-[60%] 2xl:ml-[65%] md:ml-[70%] mt-10 hover:bg-molduraBlue hover:flex hover:items-center hover:justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body text-xs">
+		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-10">
 			queremos viver aposentados no interior da inglaterra
 		</p>
 	</div>
@@ -101,7 +101,7 @@
 	<div
 		class="group border-[1px] border-casorioBlack row-start-1 row-end-1 col-start-2 col-end-3 w-[50%] 2xl:ml-[80%] md:ml-[77%] mt-8 h-[70%] hover:bg-molduraRed hover:flex hover:items-center hover:justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body text-xs">
+		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-10">
 			Nossa cor favorita é o vermelho
 		</p>
 	</div>
@@ -110,16 +110,16 @@
 		class="group border-[1px] border-casorioBlack row-start-1 row-end-4 col-start-3 col-end-3 w-[40%] 2xl:ml-[45%] md:ml-[42%] h-[50%] mt-[25%] hover:bg-molduraYellow hover:flex hover:items-center hover:justify-center"
 	>
 		<p
-			class="group-hover:block hidden text-white uppercase font-body text-xs pl-4 pr-4 pt-10 pb-10"
+			class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm pl-4 pr-4 pt-10 pb-10"
 		>
 			ficamos noivos em 2019 com alianças de plástico
 		</p>
 	</div>
 	<!-- Top right ovoid -->
 	<div
-		class="group border-[1px] rounded-[50%] border-casorioBlack row-start-1 row-end-3 col-start-4 col-end-4 2xl:w-[30%] md:w-[25%] 2xl:h-[50%] md:h-[50%] 2xl:ml-[5%] md:ml-0 2xl:mt-10 md:mt-8 hover:bg-molduraBlue hover:flex hover:items-center hover:justify-center"
+		class="group border-[1px] rounded-[50%] border-casorioBlack row-start-1 row-end-3 col-start-4 col-end-4 2xl:w-[30%] lg:w-[28%] xl:w-[25%] lg:h-[40%] xl:h-[50%] 2xl:ml-[5%] md:ml-0 2xl:mt-10 md:mt-8 hover:bg-molduraBlue hover:flex hover:items-center hover:justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body text-xs p-2">
+		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-2">
 			estamos a quase 8 anos juntos
 		</p>
 	</div>
@@ -127,7 +127,7 @@
 	<div
 		class="group border-[1px] border-casorioBlack row-start-2 row-end-2 col-start-4 col-end-4 w-[40%] 2xl:ml-[-2%] md:ml-[-8%] h-[80%] mt-[13%] hover:bg-casorioPink hover:flex hover:items-center hover:justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body text-xs p-3">
+		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-3">
 			temos os mesmos livros favoritos
 		</p>
 	</div>
@@ -139,7 +139,7 @@
 	<div
 		class="group border-[1px] border-casorioBlack row-start-3 row-end-4 col-start-1 col-end-1 w-[50%] h-[100%] 2xl:ml-[40%] md:ml-[50%] mt-[-10%] hover:bg-molduraRed hover:flex hover:items-center hover:justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body text-xs p-8">
+		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-8">
 			nos conhecemos na escola
 		</p>
 	</div>
@@ -147,7 +147,7 @@
 	<div
 		class="group border-[1px] border-casorioBlack row-start-2 row-end-2 col-start-2 col-end-2 w-[50%] h-[70%] 2xl:ml-[-5%] md:ml-[5%] 2xl:mt-[20%] xl:mt-[23%] lg:mt-[29%] hover:bg-casorioPink hover:flex hover:items-center hover:justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body text-xs p-4">
+		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-4">
 			colecionamos livros
 		</p>
 	</div>
@@ -155,19 +155,19 @@
 	<div
 		class="group border-[1px] border-casorioBlack row-start-3 row-end-4 col-start-2 col-end-2 w-[40%] h-[130%] 2xl:ml-[10%] md:ml-[20%] 2xl:mt-[10%] md:mt-[15%] hover:bg-molduraYellow hover:flex hover:items-center hover:justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body text-xs p-2">
+		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-2">
 			somos malucos por ecobag
 		</p>
 	</div>
 	<!-- Bottom center wide thin rectangle -->
 	<div
-		class="border-[1px] border-casorioBlack row-start-4 row-end-4 col-start-2 col-end-4 w-[30%] h-[20%] self-center justify-self-center mt-[0%] md:mt-[-8%]"
+		class="border-[1px] border-casorioBlack row-start-4 row-end-4 col-start-2 col-end-4 w-[30%] h-[20%] self-center justify-self-center mt-[-10%] md:mt-[-8%]"
 	/>
 	<!-- Bottom right wide rectangle -->
 	<div
 		class="group border-[1px] border-casorioBlack row-start-3 row-end-4 col-start-3 col-end-4 w-[80%] h-[90%] mt-[10%] 2xl:ml-[40%] md:ml-[38%] hover:bg-molduraBlue hover:flex hover:items-center hover:justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body text-xs">
+		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-10">
 			nosso gosto musical é muito diferente mas ao mesmo tempo muito igual
 		</p>
 	</div>
