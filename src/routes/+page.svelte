7443fd9dcd694090e438c6ad4411e56a85aab4b3
Maycon Oliveira <flayner5@gmail.com>
2023-01-11 14:56:34 -0300

    Fixed the layout of the home page for wide and tall iPad screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,17 +9,17 @@
 </svelte:head>
 
 <div
-	class="flex md:flex-row flex-col flex-intial items-center justify-around md:h-[80vh] mr-10 ml-10 md:mr-40 md:ml-36 max-w-[100vw] mt-[-2rem] 2xl:mt-[-4rem] xl:space-x-10 2xl:space-x-0"
+	class="flex lg:flex-row flex-col flex-intial items-center justify-around mb-8 lg:mb-0 lg:h-[80vh] mr-10 ml-10 lg:ml-0 xl:mr-40 xl:ml-36 max-w-[100vw] mt-[-2rem] md:mt-[-4rem] lg:mt-[-4rem] xl:mt-[-2rem] 2xl:mt-[-4rem] xl:space-x-10 2xl:space-x-0"
 >
-	<div class="md:pl-10 mb-4 md:mb-0">
+	<div class="lg:pl-10 mb-4 lg:mb-8 xl:mb-0">
 		<img
 			src="/home_img.png"
 			alt="Ilustração das nossas alianças de noivado"
-			class="h-full xl:w-[40em] 2xl:w-auto 2xl:h-[40em]"
+			class="h-full md:w-[40em] lg:w-[45em] xl:w-[40em] 2xl:w-auto 2xl:h-[40em]"
 		/>
 	</div>
 	<div
-		class="w-full md:pl-10 md:w-1/2 2xl:w-2/5 flex justify-between flex-col text-xs md:text-sm tracking-[0.09em] 2xl:leading-5 font-body text-casorioBlack uppercase"
+		class="w-full md:w-[70%] lg:pl-8 lg:mr-4 xl:mr-0 xl:pl-10 lg:w-1/2 2xl:w-2/5 flex justify-between flex-col text-xs xl:text-sm tracking-[0.09em] 2xl:leading-5 font-body text-casorioBlack uppercase"
 	>
 		<p class="mb-6">
 			Após quase oito anos, finalmente vamos nos casar! Estamos muito felizes com a chegada deste
