d66cfc9cacf06e1404612c955907e361dec3cbb8
Maycon Oliveira <flayner5@gmail.com>
2023-01-11 12:36:02 -0300

    Fixed the sizing of the homepage image for xl and 2xl desktop windows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,23 +9,29 @@
 </svelte:head>
 
 <div
-	class="flex md:flex-row flex-col flex-intial items-center justify-around md:h-[80vh] mr-10 ml-10 md:mr-40 md:ml-36 max-w-[100vw] mt-[-2rem] 2xl:mt-[-4rem]"
+	class="flex md:flex-row flex-col flex-intial items-center justify-around md:h-[80vh] mr-10 ml-10 md:mr-40 md:ml-36 max-w-[100vw] mt-[-2rem] 2xl:mt-[-4rem] xl:space-x-10 2xl:space-x-0"
 >
 	<div class="md:pl-10 mb-4 md:mb-0">
 		<img
 			src="/home_img.png"
 			alt="Ilustração das nossas alianças de noivado"
-			class="h-full 2xl:h-[40em]"
+			class="h-full xl:w-[40em] 2xl:w-auto 2xl:h-[40em]"
 		/>
 	</div>
-	<div class="w-full md:pl-10 md:w-1/2 2xl:w-2/5">
-		<p
-			class="text-xs md:text-sm tracking-[0.09em] 2xl:leading-5 font-body text-casorioBlack uppercase"
-		>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-			been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-			galley of type and scrambled it to make a type specimen book. It has survived not only five
-			centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+	<div
+		class="w-full md:pl-10 md:w-1/2 2xl:w-2/5 flex justify-between flex-col text-xs md:text-sm tracking-[0.09em] 2xl:leading-5 font-body text-casorioBlack uppercase"
+	>
+		<p class="mb-6">
+			Após quase oito anos, finalmente vamos nos casar! Estamos muito felizes com a chegada deste
+			momento tão aguardado, e gostaríamos de compartilhar um pouco da nossa alegria com vocês.
+			Nesse site vocês encontram alguns fatos interessantes sobre
+			<a class="text-casorioRed" href="/nos">nós</a>, podem conferir informações importantes sobre a
+			data e local do <a class="text-casorioRed" href="/casorio">casório</a> ou, se estiverem se
+			sentindo generosos, podem nos agraciar com um dos presentes da nossa
+			<a class="text-casorioRed" href="/presentes">lista</a>. Espero que possam vir nos prestigiar
+			nessa data maravilhosa!
 		</p>
+		<p class="self-end">Com amor,</p>
+		<p class="self-end">Maycon e Rafa</p>
 	</div>
 </div>
