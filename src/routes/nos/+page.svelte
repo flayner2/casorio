826a3437c19c9a01970225d8f4d54c1dc5954295
Maycon Oliveira <flayner5@gmail.com>
2023-02-09 19:38:22 -0300

    Started working on repositioning and fixing the frames in the 'NÓS' page

--- src/routes/nos/+page.svelte
+++ src/routes/nos/+page.svelte
@@ -95,21 +95,35 @@
 
 <!-- Larger screens (iPad + desktop) -->
 <div
-	class="hidden lg:grid grid-rows-4 grid-cols-4 w-full h-[80vh] 2xl:pl-20 2xl:pr-20 gap-0 cursor-default "
+	class="hidden lg:grid grid-rows-4 grid-cols-4 w-full h-[80vh] 2xl:pl-20 2xl:pr-20 gap-0 cursor-default text-white uppercase font-body"
 >
 	<!-- Top left -->
 	<div
-		class="group border-[1px] border-casorioBlack row-start-1 row-end-3 col-start-1 col-end-2 w-[95%] h-[60%] 2xl:ml-[65%] md:ml-[70%] mt-10 hover:bg-molduraBlue hover:flex hover:items-center hover:justify-center transition-all"
+		class="group row-start-1 row-end-3 col-start-1 col-end-2 w-[24em] h-[16em] 2xl:ml-[65%] md:ml-[70%] mt-2 flex items-center justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-10">
+		<img
+			src="./Galeria_inglaterra.png"
+			alt="Ilustração de paisagem do interior da Inglaterra"
+			class="group-hover:opacity-50 w-full transition-all"
+		/>
+		<p
+			class="group-hover:opacity-100 opacity-0 absolute flex items-center justify-center md:text-xs 2xl:text-sm p-10 group-hover:bg-molduraBlue transition-all"
+		>
 			queremos viver aposentados no interior da inglaterra
 		</p>
 	</div>
 	<!-- Top center -->
 	<div
-		class="group border-[1px] border-casorioBlack row-start-1 row-end-1 col-start-2 col-end-3 w-[50%] 2xl:ml-[80%] md:ml-[77%] mt-8 h-[70%] hover:bg-molduraRed hover:flex hover:items-center hover:justify-center transition-all"
+		class="group row-start-1 row-end-1 col-start-2 col-end-3 w-[50%] 2xl:ml-[80%] md:ml-[77%] mt-8 h-[70%] flex items-center justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-10">
+		<img
+			src="./Galeria_vermelho.png"
+			alt="Ilustração de um coração"
+			class="group-hover:opacity-50 w-full transition-all"
+		/>
+		<p
+			class="group-hover:opacity-100 opacity-0 absolute flex justify-center items-center md:text-xs 2xl:text-sm p-10 transition-all group-hover:bg-molduraRed"
+		>
 			Nossa cor favorita é o vermelho
 		</p>
 	</div>
@@ -117,9 +131,7 @@
 	<div
 		class="group border-[1px] border-casorioBlack row-start-1 row-end-4 col-start-3 col-end-3 w-[40%] 2xl:ml-[45%] md:ml-[42%] h-[50%] mt-[25%] hover:bg-molduraYellow hover:flex hover:items-center hover:justify-center transition-all"
 	>
-		<p
-			class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm pl-4 pr-4 pt-10 pb-10"
-		>
+		<p class="group-hover:block hidden md:text-xs 2xl:text-sm pl-4 pr-4 pt-10 pb-10">
 			ficamos noivos em 2019 com alianças de plástico
 		</p>
 	</div>
@@ -127,16 +139,21 @@
 	<div
 		class="group border-[1px] rounded-[50%] border-casorioBlack row-start-1 row-end-3 col-start-4 col-end-4 2xl:w-[30%] lg:w-[30%] xl:w-[25%] lg:h-[40%] xl:h-[50%] 2xl:ml-[5%] md:ml-0 2xl:mt-10 md:mt-8 hover:bg-molduraBlue hover:flex hover:items-center hover:justify-center transition-all"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-2">
-			estamos a quase 8 anos juntos
-		</p>
+		<p class="group-hover:block hidden md:text-xs 2xl:text-sm p-2">estamos a quase 8 anos juntos</p>
 	</div>
 	<!-- Center right rectangle -->
 	<div
-		class="group border-[1px] border-casorioBlack row-start-2 row-end-2 col-start-4 col-end-4 w-[40%] 2xl:ml-[-2%] md:ml-[-8%] h-[80%] mt-[13%] hover:bg-casorioPink hover:flex hover:items-center hover:justify-center transition-all"
+		class="group row-start-2 row-end-2 col-start-4 col-end-4 lg:w-[6.5em] lg:h-[6.5em] 2xl:w-[10em] 2xl:h-[10em] 2xl:ml-[-2%] md:ml-[-8%] mt-[13%]  flex items-center justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-3">
-			temos os mesmos livros favoritos
+		<img
+			src="./Galeria_xadrez.png"
+			alt="Ilustração de tabuleiro de xadrez"
+			class="group-hover:opacity-50 h-full transition-all"
+		/>
+		<p
+			class="absolute group-hover:opacity-100 opacity-0 flex justify-center items-center w-[8.75em] h-[8.75em] md:text-xs 2xl:text-sm p-3 group-hover:bg-casorioPink transition-all"
+		>
+			jogamos xadrez nas horas vagas
 		</p>
 	</div>
 	<!-- Center ovoid -->
@@ -147,24 +164,36 @@
 	<div
 		class="group border-[1px] border-casorioBlack row-start-3 row-end-4 col-start-1 col-end-1 w-[50%] h-[100%] 2xl:ml-[40%] md:ml-[50%] mt-[-10%] hover:bg-molduraRed hover:flex hover:items-center hover:justify-center transition-all"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-8">
-			nos conhecemos na escola
-		</p>
+		<p class="group-hover:block hidden md:text-xs 2xl:text-sm p-8">somos malucos por ecobag</p>
 	</div>
 	<!-- Center left wide rectangle -->
 	<div
-		class="group border-[1px] border-casorioBlack row-start-2 row-end-2 col-start-2 col-end-2 w-[50%] h-[70%] 2xl:ml-[-5%] md:ml-[5%] 2xl:mt-[20%] xl:mt-[23%] lg:mt-[29%] hover:bg-casorioPink hover:flex hover:items-center hover:justify-center transition-all"
+		class="group row-start-2 row-end-2 col-start-2 col-end-2 w-[10em] h-[10em] 2xl:ml-[10%] md:ml-[5%] 2xl:mt-[20%] xl:mt-[23%] lg:mt-[29%] flex items-center justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-4">
-			colecionamos livros
+		<img
+			src="./Galeria_escola.png"
+			alt="Ilustração de fachada de uma escola SESI"
+			class="group-hover:opacity-50 h-full transition-all"
+		/>
+		<p
+			class="absolute group-hover:opacity-100 opacity-0 w-[8.5em] h-[8.5em] flex justify-center items-center md:text-xs 2xl:text-sm p-4 group-hover:bg-casorioYellow transition-all"
+		>
+			nos conhecemos na escola
 		</p>
 	</div>
 	<!-- Bottom left tall rectangle -->
 	<div
-		class="group border-[1px] border-casorioBlack row-start-3 row-end-4 col-start-2 col-end-2 w-[40%] h-[130%] 2xl:ml-[10%] md:ml-[20%] 2xl:mt-[10%] md:mt-[15%] hover:bg-molduraYellow hover:flex hover:items-center hover:justify-center transition-all"
+		class="group row-start-3 row-end-4 col-start-2 col-end-2 w-[40%] h-[60%] 2xl:ml-[10%] md:ml-[20%] 2xl:mt-[35%] md:mt-[15%] flex items-center justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-2">
-			somos malucos por ecobag
+		<img
+			src="./Galeria_musica.png"
+			alt="Ilustração de vitrola"
+			class="group-hover:opacity-50 w-full transition-all"
+		/>
+		<p
+			class="group-hover:opacity-100 opacity-0 flex absolute items-center justify-center md:text-xs 2xl:text-sm p-2 bg-molduraYellow transition-all"
+		>
+			nosso gosto musical é muito diferente mas ao mesmo tempo muito igual
 		</p>
 	</div>
 	<!-- Bottom center wide thin rectangle -->
@@ -173,10 +202,17 @@
 	/>
 	<!-- Bottom right wide rectangle -->
 	<div
-		class="group border-[1px] border-casorioBlack row-start-3 row-end-4 col-start-3 col-end-4 w-[80%] h-[90%] mt-[10%] 2xl:ml-[40%] md:ml-[38%] hover:bg-molduraBlue hover:flex hover:items-center hover:justify-center transition-all"
+		class="group row-start-3 row-end-4 col-start-3 col-end-4 w-[24em] h-[14em] mt-[10%] 2xl:ml-[40%] md:ml-[38%] flex items-center justify-center"
 	>
-		<p class="group-hover:block hidden text-white uppercase font-body md:text-xs 2xl:text-sm p-10">
-			nosso gosto musical é muito diferente mas ao mesmo tempo muito igual
+		<img
+			src="./Galeria_livros.png"
+			alt="Ilustração de um livro aberto"
+			class="w-auto group-hover:opacity-50 transition-all"
+		/>
+		<p
+			class="absolute group-hover:opacity-100 opacity-0 w-[15%] h-[17%] flex items-center justify-center group-hover:bg-molduraBlue md:text-xs 2xl:text-sm p-10 transition-all"
+		>
+			colecionamos livros
 		</p>
 	</div>
 </div>
